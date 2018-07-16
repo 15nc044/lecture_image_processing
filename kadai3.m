573bd635a2b1f27259d27f3aa3d17381716eff4f
15nc044 <39053862+15nc044@users.noreply.github.com>
2018-07-16 14:42:40 +0900

    Update kadai3.m

--- kadai3.m
+++ kadai3.m
@@ -26,8 +26,52 @@ pause;
 IMG = ORG > 192;
 imagesc(IMG); colormap(gray); colorbar;
 
+標準画像「IMG_5297.jpg」を原画像とする．この画像はディジタルカラー画像である．
+ORG=imread('IMG_5297.jpg'); % 原画像の入力
+imagesc(ORG); axis image; % 画像の表示
+によって，原画像を読み込み，表示した結果を図１に示す．
 https://raw.githubusercontent.com/15nc044/lecture_image_processing/master/3_1.jpg
+図1 原画像
+
+以下のように輝度値が64以上の画素を1，その他を0に変換する
+IMG = ORG > 64; 
+imagesc(IMG); colormap(gray); colorbar;
+pause;
+結果を図２に示す．
+
 https://raw.githubusercontent.com/15nc044/lecture_image_processing/master/3_2.jpg
+図2 輝度値変換64
+
+以下のように輝度値が96以上の画素を1，その他を0に変換する
+IMG = ORG > 96; 
+imagesc(IMG); colormap(gray); colorbar;
+pause;
+結果を図３に示す．
+
 https://raw.githubusercontent.com/15nc044/lecture_image_processing/master/3_3.jpg
+図3 輝度値変換96
+
+以下のように輝度値が128以上の画素を1，その他を0に変換する
+IMG = ORG > 128; 
+imagesc(IMG); colormap(gray); colorbar;
+pause;
+結果を図4に示す．
+
 https://raw.githubusercontent.com/15nc044/lecture_image_processing/master/3_4.jpg
+図3 輝度値変換128
+
+以下のように輝度値が192以上の画素を1，その他を0に変換する
+IMG = ORG > 192; 
+imagesc(IMG); colormap(gray); colorbar;
+pause;
+結果を図5に示す．
+
 https://raw.githubusercontent.com/15nc044/lecture_image_processing/master/3_5.jpg
+図3 輝度値変換192
+
+これらの処理を確認し輝度値による画素変換の概要を理解できた
+
+
+
+
+
