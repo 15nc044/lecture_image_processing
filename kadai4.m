d3d66728da1d11ab7571018f6e0a0296f451c66e
15nc044 <39053862+15nc044@users.noreply.github.com>
2018-07-16 15:29:08 +0900

    Update kadai4.m

--- kadai4.m
+++ kadai4.m
@@ -12,8 +12,19 @@ pause;
 
 imhist(ORG); % ヒストグラムの表示
 
-
-
-
+標準画像「IMG_5305_edited.jpg」を原画像とする．この画像はディジタルカラー画像である．
+ORG=rgb2gray(ORG); % カラー画像を白黒濃淡画像へ変換
+imagesc(ORG); colormap(gray); colorbar;
+によって，原画像を白黒濃淡画像へと変換する，表示した結果を図１に示す．
 https://raw.githubusercontent.com/15nc044/lecture_image_processing/master/4_1.jpg
+図1 白黒濃淡画像
+
+imhist(ORG); 
+によってヒストグラムを表示する
+結果を図２に示す．
 https://raw.githubusercontent.com/15nc044/lecture_image_processing/master/4_2.jpg
+図2 ヒストグラム表示
+
+
+ディジタルカラー画像を白黒濃淡画像へと変換しそのヒストグラムを観測することによってその概要を理解できた
+
